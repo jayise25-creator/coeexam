257e8acc2cf1fa9c755ec1ad42dbde01700aa5a0
jayise25-creator <jay.ise25@cmrit.ac.in>
2026-05-13 16:00:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,47 @@
-# app.py
-
 import streamlit as st
 from PIL import Image
 import numpy as np
-from tensorflow.keras.models import load_model
+import tensorflow as tf
 
-# Page settings
-st.set_page_config(page_title="Animal Detector", layout="centered")
+st.set_page_config(page_title="Animal Detector")
 
 st.title("🐾 Animal Detector")
-st.write("Upload an animal image and detect the animal.")
 
 # Load model
-model = load_model("keras_model.h5", compile=False)
+model = tf.keras.models.load_model(
+    "keras_model.h5",
+    compile=False
+)
 
 # Load labels
 class_names = open("labels.txt", "r").readlines()
 
 # Upload image
 uploaded_file = st.file_uploader(
-    "Upload an Image",
+    "Upload Animal Image",
     type=["jpg", "jpeg", "png"]
 )
 
 if uploaded_file is not None:
 
-    # Open image
     image = Image.open(uploaded_file).convert("RGB")
 
-    # Show image
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    st.image(image, use_container_width=True)
 
-    # Detect button
     if st.button("Detect Animal"):
 
-        # Resize image
+        # Resize
         image = image.resize((224, 224))
 
-        # Convert image to array
+        # Convert to array
         image_array = np.asarray(image)
 
-        # Normalize image
+        # Normalize
         normalized_image_array = (
             image_array.astype(np.float32) / 127.5
         ) - 1
 
-        # Prepare data
+        # Prepare input
         data = np.ndarray(
             shape=(1, 224, 224, 3),
             dtype=np.float32
@@ -53,20 +49,14 @@ if uploaded_file is not None:
 
         data[0] = normalized_image_array
 
-        # Prediction
+        # Predict
         prediction = model.predict(data)
 
-        # Get index
         index = np.argmax(prediction)
 
-        # Get class name
         class_name = class_names[index]
 
-        # Confidence score
         confidence_score = prediction[0][index]
-
-        # Show result
-        st.subheader("Prediction Result")
 
         st.success(
             f"Animal: {class_name[2:].strip()}"
